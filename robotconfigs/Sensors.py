e38de69357b424d28f1b526c8a2f4bbd10b335f1
Mohammad Mehrnia <qermezkon@gmail.com>
2021-09-07 08:33:08 +0430

    Improve Sensor Detection Class

--- robotconfigs/Sensors.py
+++ robotconfigs/Sensors.py
@@ -1,23 +1,29 @@
 import random
 
 
+# Class Detection
 class DetectSensor:
+    # Class Initialize
     def __init__(self):
         self.__sendor_accuracy = 0
         self.__sendor_data = -1
 
+    # Read From Private Variable
     def get_sensor_accuarcy(self):
         return self.__sendor_accuracy
 
+    # Write Into Private Method
     def set_sensor_accuarcy(self, sendor_accuracy):
         if sendor_accuracy < 70:
             self.__sendor_accuracy = 90
         else:
             self.__sendor_accuracy = sendor_accuracy
 
+    # Read From Private Variable
     def get_sendor_data(self):
         return self.__sendor_data
 
+    # Send Pulse Detection To Sensor
     def sense(self):
         if random.random() <= self.sendor_accuracy / 100:
             # Correct Detection
