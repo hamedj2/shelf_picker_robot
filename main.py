96be102c850bf7e731e6f2583aa2e4d24cdedf16
Mohammad Mehrnia <qermezkon@gmail.com>
2021-09-13 07:39:49 +0430

    Improve

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
     result = []
     orders_list = range(1, 10)
     path_trace = []
+
     for i in range(1, 1000):
         order_count = randint(1, 10)
         orders = list(np.random.permutation(np.arange(1, 10))[:order_count])
         score, start_position, total_path = walk.walk_simulate(start_position, orders, False)
         result.append(score)
-        path_trace.append(total_path.__len__())
-
-        # print(f"Average Score is: {score}")
-        # print(f"Order Count is: {orders.__len__()}")
+        path_trace.append(total_path)
 
     print(f"Average Score is: {np.average(result)}")
     print(f"Min Score is: {np.min(result)}")
     print(f"Max Score is: {np.max(result)}")
-    print(f"Min Path is: {min(path_trace)}")
-    print(f"Max Path is: {max(path_trace)}")
+    print(f"Min Path Length is: {min(map(len, path_trace))}")
+    print(f"Min Path is: {path_trace[path_trace.index(min(path_trace, key=len))]}")
+    print(f"Max Path Length is: {max(map(len, path_trace))}")
+    print(f"Max Path is: {path_trace[path_trace.index(max(path_trace, key=len))]}")
